220bd8536b03646bf09cefc5b3736921967593c5
OliverTheLittleLion <bjohnson5@ftstudent.org>
2025-02-25 14:09:28 -0500

    worked on timer

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,8 +1,13 @@
-extends Control
-@onready var rich_text_label : RichTextLabel = $RichTextLabel
-var timeL = 54000
+extends Node
+@onready var timer = $Timer
+@onready var label = $Label
+var isTimerStarted = false
 func _ready():
-    if timeL > 0:
-    if Input.is_key_pressed(Key.A):
-        timeL -=1
-        rich_text_label.bbcode_text = "TIME LEFT: %d" % timeL
+	timer.stop()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept") and !isTimerStarted:
+		timer.start(900)
+func _on_timer_timeout():
+
+	pass # Replace with function body.
